d09cbd447c02dce2586e39463b01e97f296d1126
vinhig <vincent.higginson@student.unamur.Be>
2019-04-15 14:08:42 +0200

    Init base crystal project

---
+++ spec/endiver_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Endiver do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/endiver.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Endiver`
+module Endiver
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
